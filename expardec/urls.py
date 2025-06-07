7ea3dbbe9d62ef06db0d9994ace1a249462c3c41
NirutG <nirut.system@gmail.com>
2025-06-07 23:41:23 +0700

    Modified some content for website is under construction!

--- expardec/urls.py
+++ expardec/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ]
 
 # # Serve static files in development
